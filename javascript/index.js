7a99d0f331e90509bddda4b4139ef86b655a14ae
Matías Viñoles <vinolesmatias@gmail.com>
2022-09-19 21:54:13 -0300

    index fuera de pages

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,3 @@
-//class Producto {
-//    constructor(nombre, precio, id){
-//        this.nombre = nombre;
-//        this.precio = precio; 
-//        this.id = id;   
-//    }
-//}
-//
-//class ItemCarrito {
-//    constructor(id, cantidad){
-//        this.id = id;
-//        this.cantidad = cantidad;
-//    }
-//}
-//let carrito = []
-//
-//let productos = [new Producto("Curso de Guitarra", 9500, 1),
-//                 new Producto("Curso de Bajo", 10000, 2),
-//                 new Producto("Curso de Teclado", 11500, 3),
-//                 new Producto("Clase individual de Guitarra", 850, 4),
-//                 new Producto("Clase individual de Bajo", 950, 5),
-//                 new Producto("Clase individual de Teclado", 1100, 6)]
-//
-
-
-
 //Interactividad del botón Carrito
 const openShopCart = document.querySelector('.boton-carrito');
 openShopCart.addEventListener('click', () => {
